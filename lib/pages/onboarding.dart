d17cd399e4e202ba31684e8b5d7a44809f4f76cf
dionisiusragil13 <dionisiusragil13@gmail.com>
2025-06-24 21:47:51 +0700

    hari pertama

---
+++ lib/pages/onboarding.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:package_delivery_app/service/widget_support.dart';
+
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
+  @override
+  State<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Container(
+        margin: EdgeInsets.only(top: 20.0),
+        child: Column(
+          children: [
+            Image.asset("images/onboard.png"),
+            SizedBox(height: 50.0),
+            Text(
+              "Track Your Parcel\nFrom Anywhere",
+              style: AppWidget.HeadlineTextfieldStyle(),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 30.0),
+            Text(
+              "check your progress of\nyour delivery",
+              textAlign: TextAlign.center,
+              style: AppWidget.SimpleTextfieldStyle(),
+            ),
+            SizedBox(height: 30.0),
+            Material(
+              elevation: 3.0,
+              borderRadius: BorderRadius.circular(30.0),
+              child: Container(
+                width: MediaQuery.of(context).size.width / 2,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Color(0xfff8ae39),
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+                child: Center(
+                  child: Text(
+                    "Track Now",
+                    style: AppWidget.WhiteTextfieldStyle(),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/service/widget_support.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class AppWidget {
+  static TextStyle HeadlineTextfieldStyle() {
+    return TextStyle(
+      color: Colors.black,
+      fontSize: 30.0,
+      fontWeight: FontWeight.bold,
+    );
+  }
+
+  static TextStyle SimpleTextfieldStyle() {
+    return TextStyle(
+      color: Colors.black38,
+      fontSize: 18.0,
+      fontWeight: FontWeight.w500,
+    );
+  }
+  static TextStyle WhiteTextfieldStyle() {
+    return TextStyle(
+      color: Colors.white,
+      fontSize: 24.0,
+      fontWeight: FontWeight.bold,
+    );
+  }
+}
